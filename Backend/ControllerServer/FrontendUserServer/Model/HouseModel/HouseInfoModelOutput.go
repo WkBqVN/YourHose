82aad8017384c3d58fee0d18613f3cd2d684ff94
ko24 <khoavan.vo@dxc.com>
2022-11-14 17:28:49 +0700

    First Init Data

---
+++ Backend/ControllerServer/FrontendUserServer/Model/HouseModel/HouseInfoModelOutput.go
@@ -0,0 +1,10 @@
+package model
+
+type house struct {
+	houseId      int
+	country      string
+	city         string
+	district     string
+	street       string
+	streetNumber string
+}
---
+++ DataChange/Mssql/HousePrice_Add_Table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE
+    HOUSEPRICE (
+        HouseID int,
+        Timeline DATE,
+        Price int,
+    );
