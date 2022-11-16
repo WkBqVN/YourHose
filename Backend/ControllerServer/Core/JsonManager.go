f31b2eff2e0d881b832d4c659741dfd974f06d39
ko24 <khoavan.vo@dxc.com>
2022-11-16 17:07:29 +0700

    Update golang server

--- Backend/ControllerServer/Core/JsonManager.go
+++
@@ -1,7 +0,0 @@
-package core
-
-type jsonManager interface {
-	genMapObj(key string, obj map[string]string) map[string]interface{}
-	genStringObj(key string, value string) map[string]string
-	genIntObj(key string, value int) map[string]int
-}
